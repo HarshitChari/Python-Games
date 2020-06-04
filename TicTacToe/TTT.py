3eb06fa3a1c8aeec3fe1520cc10f8f725da5276f
Harshit Chari <55452858+HarshitChari@users.noreply.github.com>
2020-06-04 22:25:25 +0530

    main file
    Source File

---
+++ TicTacToe/TTT.py
@@ -0,0 +1,8 @@
+from tkinter import *
+import numpy as np
+
+size_of_board = 800
+symbol_size = (size_of_board / 3 - size_of_board / 8) / 2
+symbol_thickness = 30
+symbol_X_color = '#fff'
+symbol_O_color = '#000000'
